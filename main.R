f2b47130a5c84cd78edc65b1626a000c30a66286
Jiaye  Su <suj1@rose-hulman.edu>
2020-02-16 16:09:17 -0500

    Add data and import data

--- main.R
+++ main.R
@@ -1,3 +1,8 @@
-main()<-function(){
-  
+getData <- function(name){
+  data <- read.csv(paste0("./eurosat-w2020/", name))
+  return (data)
 }
+
+train_data <- getData("train.csv")
+test_data <- getData("test.csv")
+

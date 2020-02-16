ad91800d91fd21c8348cf59c7f8437a3d6f403df
WenqiAngieWu <wuwenqi_7@163.com>
2020-02-16 18:44:14 -0500

    Update main.R

--- main.R
+++ main.R
@@ -6,3 +6,57 @@ getData <- function(name){
 train_data <- getData("train.csv")
 test_data <- getData("test.csv")
 
+###################################
+### cleaning
+
+
+
+
+
+
+
+###################################
+library(class)
+library(ggplot2)
+
+#### Split training and test data (75%, 25%) ####
+set.seed(20200214)
+trainSize <- floor(nrow(train_data)*0.75)
+trainInx <- sample(seq_len(nrow(train_data)),size = trainSize)
+train.X <- train_data[trainInx, 2:(ncol(train_data)-1)]
+test.X <- train_data[-trainInx, 2:(ncol(train_data)-1)]
+train.y <- train_data[trainInx, ncol(train_data)]
+test.y <- train_data[-trainInx, ncol(train_data)]
+
+## knn
+k_range <- c(1:10) # range of k
+err.df <- data.frame(k = rep(0, length(k_range)),
+                     train.err = rep(0, length(k_range)),
+                     test.err = rep(0, length(k_range)))
+i <- 1
+for(k in k_range){
+  knn.test.pred <- knn(train = train.X, test = test.X, cl = train.y, k = k) # knn for test data
+  knn.train.pred <- knn(train = train.X, test = train.X, cl = train.y, k = k) # knn for training data
+  train.err <- mean(train.y != knn.train.pred) # training error
+  test.err <- mean(test.y != knn.test.pred) # test error
+  
+  # err.df each row three columns: k, training error, test error
+  err.df[i,] <- c(k, train.err, test.err)
+  
+  i <- i+1
+}
+
+
+err.df.plot <- reshape2::melt(err.df, id.vars = "k")
+ggplot(err.df.plot) + geom_line(aes(x = k, y = value,
+                                    color = variable)) +
+  geom_point(aes(x = k, y = value,
+                 color = variable)) + 
+  labs(title = "Test & Training Error", x = "k", y = "Error") +
+  theme(legend.position = "bottom",
+        plot.margin = unit(c(.2,1,.2,.2), "cm")) + 
+  scale_color_manual(name="",
+                     labels=c("Training Error","Test Error"),
+                     values=c("red","blue")) +
+  theme_bw()
+

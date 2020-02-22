fc6c1052e905d823715c42d334c0f3a674400301
WenqiAngieWu <wuwenqi_7@163.com>
2020-02-22 17:16:16 -0500

    Update convertDataType.R

--- convertDataType.R
+++ convertDataType.R
@@ -1,10 +1,13 @@
 convertDataType <- function(myData){
-  myData <- fillMissingDataByMean(getData("train.csv"))
   textDataShort <- read.csv("./eurosat-w2020/codebook_compact.csv", header = T, stringsAsFactors = F)
+  idx <- c()
   for(i in 1:nrow(textDataShort)){
     if(textDataShort[i, "Mean"] != "."){
       myData[i+1] <- as.numeric(unlist(myData[i+1]))
+    }else{
+      idx <- c(idx, i+1)
     }
   }
+  myData <- myData[,-idx]
   return (myData)
 }
